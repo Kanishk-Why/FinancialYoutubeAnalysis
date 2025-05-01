5a0db0352ea6e0f331accaf91103ebf90917ab0c
Kanishk Vij <84846105+Kanishk-Why@users.noreply.github.com>
2025-05-01 23:05:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def fetch_stock_news_from_api(ticker, api_key):
         return []
 
 # Replace with your own API key from NewsAPI
-news_api_key = '047ad87e36534422b4bf4491b9ac6a71'
+news_api_key = '63db1ccd3467473888bb20f03d2873c1'
 
 # Integrate into Streamlit for news
 st.subheader("📰 Latest News")
